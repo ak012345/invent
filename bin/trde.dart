87a37951908a1a4bc4124c88830635d2c97a7809
Kernan, Alexander P <Alexander.Kernan@gtri.gatech.edu>
2021-07-09 13:08:17 -0400

    trde functions as intended

--- bin/trde.dart
+++ bin/trde.dart
@@ -2,15 +2,16 @@ import 'comm.dart';
 import 'invent.dart';
 
 class Trde {
+  // a=4, b=1
   Trde(Comm commIn, Comm commOut, Invent theInv) {
-    Comm newTrdeInVal = new Comm(commIn.name, commOut.qty / commIn.qty);
-    Comm newTrdeOutVal = new Comm(commOut.name, commIn.qty / commOut.qty);
+    Comm newTrdeOutVal = new Comm(commOut.name, (commOut.qty / commIn.qty));
+    Comm newTrdeInVal = new Comm(commIn.name, commIn.qty / commOut.qty);
 
     Comm copyOfCommIn = new Comm.copy(commIn);
     Comm copyOfCommOut = new Comm.copy(commOut);
 
-    copyOfCommIn.addVal(newTrdeInVal);
-    copyOfCommOut.addVal(newTrdeOutVal);
+    copyOfCommIn.addVal(newTrdeOutVal);
+    copyOfCommOut.addVal(newTrdeInVal);
 
     theInv.updateInvent(copyOfCommIn, copyOfCommOut);
   }
